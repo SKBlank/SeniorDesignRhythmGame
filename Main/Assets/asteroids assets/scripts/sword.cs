00e3fedb4a3a1d8e5d566a8079fb36d9a5301a91
lyra3 <jsullenb@vols.utk.edu>
2023-03-08 17:12:22 -0500

    added keyboard movement to sword

--- Main/Assets/asteroids assets/scripts/sword.cs
+++ Main/Assets/asteroids assets/scripts/sword.cs
@@ -6,6 +6,16 @@ public class sword : MonoBehaviour
 {
     public ComboHealth comboHealth;
     public AudioSource swordSound;
+    Vector3 startPos;
+    //public Transform transform_sword = sword.transform;
+    void Start() {
+        startPos = transform.position;
+    } 
+
+    void Update() {
+        moveLeftRight();
+        moveUpDown();
+    }
 
     void OnCollisionEnter(Collision targetObj) 
     {
@@ -15,5 +25,18 @@ public class sword : MonoBehaviour
             comboHealth.IncreaseCombo();
         }
     }
+    
+    void moveLeftRight(){
+        Vector3 vecLR = Vector3.zero;
+        vecLR.z = Input.GetAxis("Horizontal");
+        Vector3 v = new Vector3(0.0f, 0.0f, vecLR.z) * Time.deltaTime * 15.0f;
+        transform.Translate(v, Space.World);
+    }    
+    void moveUpDown(){
+        Vector3 vecUD = Vector3.zero;
+        vecUD.y = Input.GetAxis("Vertical");
+        Vector3 v = new Vector3(0.0f, vecUD.y, 0.0f) * Time.deltaTime * 15.0f;
+        transform.Translate(v, Space.World);
+    }    
 }
 
