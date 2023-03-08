82898b883a5de36ad70e1da7ece143cc076c4814
Allen Hughes <ahughe51@vols.utk.edu>
2023-03-08 17:15:11 -0500

    added main menu functionality and visual consistency

---
+++ Main/Assets/asteroids assets/scripts/FirstRunScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstRunScript : MonoBehaviour
+{
+   public static class Globals {
+        public const int AsteroidScene = 1;
+        public const int RhythmScene = 2;
+   }
+}
--- Main/Assets/asteroids assets/scripts/MenuManager.cs
+++ Main/Assets/asteroids assets/scripts/MenuManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public void StartGameAsteroid() {
+        SceneManager.LoadScene(FirstRunScript.Globals.AsteroidScene);
+    }
+    
+    public void StartGameRhythm() {
+        SceneManager.LoadScene(FirstRunScript.Globals.RhythmScene);
+    }
+
     public void QuitGame() {
         Application.Quit();
     }
