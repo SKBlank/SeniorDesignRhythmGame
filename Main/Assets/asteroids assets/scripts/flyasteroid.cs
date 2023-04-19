5cd1853722a3d6f877b2d8e6a28aecceae7927d2
SKBlank <80212447+SKBlank@users.noreply.github.com>
2023-04-19 03:57:49 -0400

    fixed spawn system logic to eliminate spawning extra objects

--- Main/Assets/asteroids assets/scripts/flyasteroid.cs
+++ Main/Assets/asteroids assets/scripts/flyasteroid.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class flyasteroid : MonoBehaviour
 {
-    private Transform startPos;
-    private Transform endPos;
+    private Vector3 startPos;
+    private Vector3 endPos;
     private float TimeToEndPos = 1.0f;
     private float currentTime = 0.0f;
 
      void Start()
     {
         // Set end pos y and z to the same as start pos
-        endPos.position = new Vector3(endPos.position.x, startPos.position.y, startPos.position.z);
+        endPos = new Vector3(endPos.x, startPos.y, startPos.z);
     }
 
 
@@ -20,22 +20,20 @@ public class flyasteroid : MonoBehaviour
     {
         currentTime += Time.deltaTime;
         float t = Mathf.Clamp01(currentTime / TimeToEndPos);
-        transform.position = Vector3.Lerp(startPos.position, endPos.position, t);
+        transform.position = Vector3.Lerp(startPos, endPos, t);
 
         if (currentTime >= TimeToEndPos)
         {
             currentTime = 0.0f;
-            startPos.position = endPos.position;
-            endPos.position = new Vector3(endPos.position.x + 10, endPos.position.y, endPos.position.z);
+            startPos = endPos;
+            endPos = new Vector3(endPos.x + 10, endPos.y, endPos.z);
         }
     }
 
     // Set start and end positions for the asteroid object
     public void SetStartAndEndPositions(Vector3 start, Vector3 end)
     {
-        startPos = new GameObject().transform;
-        startPos.position = start;
-        endPos = new GameObject().transform;
-        endPos.position = new Vector3(end.x, start.y, start.z);
+        startPos = start;
+        endPos = new Vector3(end.x, start.y, start.z);
     }
 }
