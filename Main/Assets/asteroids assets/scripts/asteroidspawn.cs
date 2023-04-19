0be184a495b8ed7629c24f672fb3d90a88a9e749
phoenix <SusanBlankenship22@Gmail.com>
2023-04-19 02:21:41 -0400

    closes #21 timing events precisely to audio is very difficult in unity.  I ended up implementing a system that samples a video to map out events in time to the music as advised by @bbible3.  It also included an updated spawning system into a grid that is mapped onto by the video sampler.  I also updated the flyasteroid script to precisely have a set start and end point and given amount of time in between so timing offset for the beat map would be precise as well as keep the asteroids on their given paths.  To reflect this, I also updated the spawner to instantiate new values associated with the updated flyasteroid script.  The skybox is currently set to a bright debugging version for testing.

--- Main/Assets/asteroids assets/scripts/asteroidspawn.cs
+++ Main/Assets/asteroids assets/scripts/asteroidspawn.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 public class asteroidspawn : MonoBehaviour
 {
     public GameObject[] asteroids;
+    public GameObject endPoint;
 
     void Start()
     {
         // print("started aster spawn")
-        InvokeRepeating("spawnAsteroid", 1f, 3f);
+        // InvokeRepeating("spawnAsteroid", 1f, 3f);
+        //spawn one asteroid
+        // spawnAsteroid();
+    }
+
+    void OnEnable()
+    {
+        spawnAsteroid();
     }
 
     void spawnAsteroid()
     {
         print("spawned aster");
         // Debug.Log("spawned aster");
-        Vector3 randomPosition = new Vector3(0, Random.Range(-3, 3), Random.Range(-3, 3));
+        // Vector3 randomPosition = new Vector3(0, Random.Range(-3, 3), Random.Range(-3, 3));
+        // Vector3 randomPosition = new Vector3(0, 0, 0);
         int prefabIndex = Random.Range(0, asteroids.Length);
 
-        Instantiate(asteroids[prefabIndex], randomPosition, Quaternion.identity);
+        // Instantiate(asteroids[prefabIndex], transform.position, Quaternion.identity);
         // Instantiate(asteroids[prefabIndex], randomPosition, Random.Range(0, 360) * Quaternion.identity);
+
+        GameObject asteroidInstance = Instantiate(asteroids[prefabIndex], transform.position, Quaternion.identity);
+        flyasteroid flyasteroid = asteroidInstance.GetComponent<flyasteroid>();
+
+        // Set unique start and end positions for the flyasteroid script
+        //star position is the current position of the asteroid
+        Vector3 startPos = transform.position;
+
+        Vector3 endPos = endPoint.transform.position;
+
+        flyasteroid.SetStartAndEndPositions(startPos, endPos);
     }
 
     void Update()
