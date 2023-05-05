6f8203498d3223ba81032101d962d48a64bd082a
FueledByPants <ahughe51.vols.utk.edu>
2023-05-05 11:59:37 -0400

    staging 3

--- Main/Assets/asteroids assets/scripts/BeatmapDeploy.cs
+++ Main/Assets/asteroids assets/scripts/BeatmapDeploy.cs
@@ -10,19 +10,40 @@ public class BeatmapDeploy : MonoBehaviour
 
     public VideoPlayer player;
 
-    public void chooseMap() {
-        if(PlayerPrefs.GetInt("difficulty") == 0 && PlayerPrefs.GetInt("difficulty") == 0) {
-            player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_ez_-_short.mp4");
-        }
-        else if (PlayerPrefs.GetInt("difficulty") == 0 && PlayerPrefs.GetInt("extended") == 1) {
-            player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_ez.mp4");
-        }
-        else if (PlayerPrefs.GetInt("difficulty") == 1 && PlayerPrefs.GetInt("extended") == 0) {
-            player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_hard_-_short.mp4");
+    public int sceneNum;
+
+    public void start() {
+        if(sceneNum == 1) {
+            if(PlayerPrefs.GetInt("difficulty") == 0 && PlayerPrefs.GetInt("difficulty") == 0) {
+                player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_ez_-_short.mp4");
+            }
+            else if (PlayerPrefs.GetInt("difficulty") == 0 && PlayerPrefs.GetInt("extended") == 1) {
+                player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_ez.mp4");
+            }
+            else if (PlayerPrefs.GetInt("difficulty") == 1 && PlayerPrefs.GetInt("extended") == 0) {
+                player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_hard_-_short.mp4");
+            }
+            else {
+                player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_hard_1.mp4");
+            }
+
+            player.Play();
         }
         else {
-            player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_hard_1.mp4");
+            if(PlayerPrefs.GetInt("difficulty") == 0 && PlayerPrefs.GetInt("difficulty") == 0) {
+                player.clip = Resources.Load<VideoClip>("Main/Assets/New arrow assets/arrow maps/pixels_new_arrows_easy_-_short.mp4");
+            }
+            else if (PlayerPrefs.GetInt("difficulty") == 0 && PlayerPrefs.GetInt("extended") == 1) {
+                player.clip = Resources.Load<VideoClip>("Main/Assets/New arrow assets/arrow maps/pixels new arrows easy 5s offset.mp4");
+            }
+            else if (PlayerPrefs.GetInt("difficulty") == 1 && PlayerPrefs.GetInt("extended") == 0) {
+                player.clip = Resources.Load<VideoClip>("Main/Assets/New arrow assets/arrow maps/pixels_new_arrows_hard_-_short_.mp4");
+            }
+            else {
+                player.clip = Resources.Load<VideoClip>("Assets/New arrow assets/arrow maps/pixels new arrows hard.mp4");
+            }
+
+            player.Play();
         }
-         
     }
 }
