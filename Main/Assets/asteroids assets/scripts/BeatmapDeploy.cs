a3d4b5a30e93bd03f3760a2c396e1fd895c04255
FueledByPants <ahughe51.vols.utk.edu>
2023-05-05 11:04:54 -0400

    staging 2

--- Main/Assets/asteroids assets/scripts/BeatmapDeploy.cs
+++ Main/Assets/asteroids assets/scripts/BeatmapDeploy.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using System;
 
 public class BeatmapDeploy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public VideoPlayer player;
+
+    public void chooseMap() {
+        if(PlayerPrefs.GetInt("difficulty") == 0 && PlayerPrefs.GetInt("difficulty") == 0) {
+            player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_ez_-_short.mp4");
+        }
+        else if (PlayerPrefs.GetInt("difficulty") == 0 && PlayerPrefs.GetInt("extended") == 1) {
+            player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_ez.mp4");
+        }
+        else if (PlayerPrefs.GetInt("difficulty") == 1 && PlayerPrefs.GetInt("extended") == 0) {
+            player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_hard_-_short.mp4");
+        }
+        else {
+            player.clip = Resources.Load<VideoClip>("Main/Assets/asteroids assets/video/pixels_aster_hard_1.mp4");
+        }
+         
     }
 }
